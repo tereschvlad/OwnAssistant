67ab87d81e48047d9910defe56df0cd93dd41d98
tereschvlad <117782342+tereschvlad@users.noreply.github.com>
2024-01-03 01:54:34 +0200

    add repository methods

---
+++ OwnAssistantCommon/Interfaces/IDbRepository.cs
@@ -0,0 +1,11 @@
+﻿using OwnAssistantCommon.Models;
+
+namespace OwnAssistantCommon.Interfaces
+{
+    public interface IDbRepository
+    {
+        Task AddUserAsync(User user);
+
+        Task AddTasksAsync(List<CustomerTask> tasks);
+    }
+}
---
+++ OwnAssistantCommon/Models/DbRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using OwnAssistantCommon.Interfaces;
+
+namespace OwnAssistantCommon.Models
+{
+    public class DbRepository : IDbRepository
+    {
+        private readonly ILogger<DbRepository> _logger;
+        private readonly DataContext _context;
+
+        public DbRepository(ILogger<DbRepository> logger, DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public async Task AddTasksAsync(List<CustomerTask> tasks)
+        {
+            await _context.AddRangeAsync(tasks);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
